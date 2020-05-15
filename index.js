a362a9f8601e687912b57edae7ce48f060b9d7f7
mdsabur <mdsabur@gmail.com>
2020-05-15 18:02:31 +0600

    2 files modified

--- index.js
+++ index.js
@@ -3,34 +3,66 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'Md Abdus Sabur'
-    };
-  }
-  componentDidMount() {
-  this.getUsers();
-}
+class App extends React.Component {
+  state = {
+    users: [],
+    isLoading: true,
+    errors: null
+  };
 
   getUsers() {
-  axios
-    .get("http://schoolbook.bizworldbd.com/hello.php")
-    .then(response => console.log(response))
-    .catch(error => this.setState({ error, isLoading: false }));
-}
+    axios
+      .get("https://randomuser.me/api/?results=5")
+      .then(response =>
+        response.data.results.map(user => ({
+          name: `${user.name.first} ${user.name.last}`,
+          username: `${user.login.username}`,
+          email: `${user.email}`,
+          image: `${user.picture.thumbnail}`
+        }))
+      )
+      .then(users => {
+        this.setState({
+          users,
+          isLoading: false
+        });
+      })
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
 
   render() {
+    const { isLoading, users } = this.state;
     return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
+      <React.Fragment>
+        <h2>Random User</h2>
+        <div>
+          {!isLoading ? (
+            users.map(user => {
+              const { username, name, email, image } = user;
+              return (
+                <div key={username}>
+                  <p>{name}</p>
+                  <div>
+                    <img src={image} alt={name} />
+                  </div>
+                  <p>{email}</p>
+                  <hr />
+                </div>
+              );
+            })
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      </React.Fragment>
     );
   }
 }
+
+
 
 render(<App />, document.getElementById('root'));
